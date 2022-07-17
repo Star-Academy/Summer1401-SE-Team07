7602a29868cff2df6b85dfb46131ca5b4d4a5fa3
Taha Alikhani <31014328+Taha-Alikhani@users.noreply.github.com>
2022-07-17 20:43:27 +0430

    Implemented full search and completed Q2
    Cleaning up further

--- Star Practices/src/se01/FileReaderClass.java
+++ Star Practices/src/se01/FileReaderClass.java
@@ -9,7 +9,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FileReaderClass {
-
+    // Words that are used too often in english and take too much storage to index,
+    // and are practically useless
     private List<String> stopWords = Arrays.asList("a", "able", "about",
             "across", "after", "all", "almost", "also", "am", "among", "an",
             "and", "any", "are", "as", "at", "be", "because", "been", "but",
@@ -25,55 +26,76 @@ public class FileReaderClass {
             "tis", "to", "too", "twas", "us", "wants", "was", "we", "were",
             "what", "when", "where", "which", "while", "who", "whom", "why",
             "will", "with", "would", "yet", "you", "your");
+
+    // blacklist characters to remove from a word
     private char[] blacklist = "!$^()_+=-.;,[]{}\'\"".toCharArray();
 
-    private List<Pair<String,Integer>> tokens = new ArrayList<Pair<String,Integer>>();
+    private List<Pair<String, Integer>> tokens = new ArrayList<Pair<String, Integer>>();
     private List<File> m_files;
 
+    private String trimWord(String _word){
+        // remove blacklist characters
+        String word = _word.toUpperCase();
+        for (char c : blacklist)
+            word = word.replace(c, '\0');
+        return word;
+    }
+
+    private void readFile(File file, int index) {
+        // Reads a file and adds the tokens to the list
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                for (String _word : line.split("\\W+")) {
+                    String word = trimWord(_word);
+                    if (stopWords.contains(word))
+                        continue;
+                    Pair<String, Integer> token = new Pair<String, Integer>(word, index);
+                    tokens.add(token);
+                }
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public FileReaderClass(File directory) {
+        // Creates a list of files in the directory, and reads them
         List<File> files = ReadDirectory(directory);
         m_files = files;
         for (int i = 0; i < files.size(); i++) {
             File file = files.get(i);
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(file));
-                for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-                    for (String _word : line.split("\\W+")) {
-                        String word = _word.toUpperCase();
-                        for (char c : blacklist)
-                            word = word.replace(c, '\0');
-                        if (stopWords.contains(word))
-                            continue;
-                        Pair<String,Integer> token = new Pair<String,Integer>(word, i);
-                        tokens.add(token);
-                    }
-                }
-                reader.close();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
+            readFile(file, i);
+            
         }
-
     }
 
     private FilenameFilter txtFilter = new FilenameFilter() {
+        // Filters out all files that are not .txt files
         public boolean accept(File dir, String name) {
             return name.toLowerCase().endsWith(".txt");
         }
     };
 
     public List<File> ReadDirectory(File directory) {
+        // Returns a list of text files in the directory
         File[] files = directory.listFiles(txtFilter);
         return Arrays.asList(files);
     }
 
-    public List<Pair<String,Integer>> get_tokens() {
-        return new ArrayList<Pair<String,Integer>>(tokens);
+    public List<Pair<String, Integer>> get_tokens() {
+        // Returns the list of tokens
+        return new ArrayList<Pair<String, Integer>>(tokens);
     }
 
     public List<File> get_files() {
+        // Returns the list of files
         return new ArrayList<File>(m_files);
+    }
+
+    public int get_file_count() {
+        // Returns the number of files
+        return m_files.size();
     }
 }
--- Star Practices/src/se01/InvertedIndex.java
+++ Star Practices/src/se01/InvertedIndex.java
@@ -9,18 +9,72 @@ import java.util.List;
 public class InvertedIndex {
 
     private Map<String, Set<Integer>> index = new HashMap<String, Set<Integer>>();
+    private int m_fileCount;
 
-    public InvertedIndex(List<Pair<String, Integer>> tokens) {
+    // creating the inverted index
+    public InvertedIndex(List<Pair<String, Integer>> tokens, int fileCount) {
+        m_fileCount = fileCount;
         for (Pair<String, Integer> token : tokens) {
             if (index.get(token.getKey()) == null) {
                 index.put(token.getKey(), new HashSet<Integer>());
             }
             index.get(token.getKey()).add(token.getValue());
         }
+    }
 
+    public Set<Integer> applySearchQuery(Set<String> mandatory, Set<String> optional, Set<String> exclude) {
+        // Applies the search query to include all mandatory words, include at least one optional word, and exclude all exclude words.
+
+        Set<Integer> result = new HashSet<>();
+        Set<Integer> opt_result = new HashSet<>();
+        for (int i = 0; i < m_fileCount; i++) {
+            result.add(i);
+        }
+        
+        // going through all the sets and applying appropriate search query
+        for (String word : mandatory) {
+            Set<Integer> docs = index.get(word);
+            if (docs != null)
+                result.retainAll(docs);
+        }
+        for (String word : exclude) {
+            Set<Integer> docs = index.get(word);
+            if (docs != null)
+                result.removeAll(docs);
+        }
+        for (String word : optional) {
+            Set<Integer> docs = index.get(word);
+            if (docs != null)
+                opt_result.addAll(docs);
+        }
+        // checking for empty optional queries
+        if (!optional.isEmpty()) {
+            result.retainAll(opt_result);
+        }
+        return result;
     }
 
     public Set<Integer> search(String Query) {
-        return index.get(Query.toUpperCase());
+        Set<String> mandatory = new HashSet<String>();
+        Set<String> optional = new HashSet<String>();
+        Set<String> exclude = new HashSet<String>();
+
+        String[] words = Query.split(" ");
+        for (String word : words) {
+            // Flag for mandatory, optional or exclude word and add to the respective set
+            word = word.toUpperCase();
+            if (word.startsWith("+")) {
+                word = word.substring(1);
+                optional.add(word);
+            } else if (word.startsWith("-")) {
+                word = word.substring(1);
+                exclude.add(word);
+            } else {
+                mandatory.add(word);
+            }
+        }
+        Set<Integer> result = applySearchQuery(mandatory, optional, exclude);
+
+        return result;
     }
 }
