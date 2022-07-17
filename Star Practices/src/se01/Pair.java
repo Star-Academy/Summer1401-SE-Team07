d8dd9ac965ae3903e5c04514cdb044f334fff979
nikan zamani <zamaninikan@gmail.com>
2022-07-17 16:26:42 +0430

    adding pair class

---
+++ Star Practices/src/se01/Pair.java
@@ -0,0 +1,11 @@
+package se01;
+
+public class Pair {
+    public String word;
+    public int fileno;
+
+    public Pair(String word, int fileno) {
+        this.word = word;
+        this.fileno = fileno;
+    }
+}
