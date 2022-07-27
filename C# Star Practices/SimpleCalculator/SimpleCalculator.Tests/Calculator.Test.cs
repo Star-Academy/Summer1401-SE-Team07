4c31779bcdccd4ad87d0b677c7c2b46db6011573
Taha Alikhani <31014328+Taha-Alikhani@users.noreply.github.com>
2022-07-27 22:27:09 +0430

    seperating 4 simple operations tests

--- C# Star Practices/SimpleCalculator/SimpleCalculator.Tests/Calculator.Test.cs
+++ C# Star Practices/SimpleCalculator/SimpleCalculator.Tests/Calculator.Test.cs
@@ -4,20 +4,25 @@ using Xunit;
 using SimpleCalculator.Business;
 using SimpleCalculator.Business.Enums;
 
-public class CalculatorTest
+public class CalculatorSumTest
 {
+    Calculator calculator = new Calculator();
+
     [Theory]
     [InlineData(1, 2, 3)]
     [InlineData(2, 2, 4)]
     [InlineData(3, -2, 1)]
     public void Add_TwoNumbers_ReturnsSum(int a, int b, int expected)
     {
-        var calculator = new Calculator();
-
         var result = calculator.Calculate(a, b, OperatorEnum.sum);
 
         Assert.Equal(expected, result);
     }
+}
+
+public class CalculatorSubtractTest
+{
+    Calculator calculator = new Calculator();
 
     [Theory]
     [InlineData(1, 2, -1)]
@@ -25,13 +30,16 @@ public class CalculatorTest
     [InlineData(3, 2, 1)]
     public void Subtract_TwoNumbers_ReturnsDifference(int a, int b, int expected)
     {
-        var calculator = new Calculator();
-
         var result = calculator.Calculate(a, b, OperatorEnum.sub);
 
         Assert.Equal(expected, result);
     }
+}
 
+public class CalculatorMultiplyTest
+{
+    Calculator calculator = new Calculator();
+    
     [Theory]
     [InlineData(1, 2, 2)]
     [InlineData(2, 2, 4)]
@@ -39,21 +47,22 @@ public class CalculatorTest
     [InlineData(3, 2, 6)]
     public void Multiply_TwoNumbers_ReturnsProduct(int a, int b, int expected)
     {
-        var calculator = new Calculator();
-
         var result = calculator.Calculate(a, b, OperatorEnum.multiply);
 
         Assert.Equal(expected, result);
     }
+}
 
+public class CalculatorDivisonTest
+{
+    Calculator calculator = new Calculator();
+    
     [Theory]
     [InlineData(1, 2, 0.5)]
     [InlineData(2, 2, 1)]
     [InlineData(3, 2, 1.5)]
     public void Divide_TwoNumbers_ReturnsQuotient(int a, int b, double expected)
     {
-        var calculator = new Calculator();
-
         var result = calculator.Calculate(a, b, OperatorEnum.division);
 
         Assert.Equal(expected, result);
