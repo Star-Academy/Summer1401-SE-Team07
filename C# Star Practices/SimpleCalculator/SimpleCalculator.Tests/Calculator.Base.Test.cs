47d07477de039ad76af6ee4f0e25971266065f4a
Taha Alikhani <31014328+Taha-Alikhani@users.noreply.github.com>
2022-07-29 15:23:08 +0430

    testing base operators

---
+++ C# Star Practices/SimpleCalculator/SimpleCalculator.Tests/Calculator.Base.Test.cs
@@ -0,0 +1,71 @@
+namespace SimpleCalculator.Tests;
+
+using SimpleCalculator.Business;
+using SimpleCalculator.Business.Enums;
+
+// test Calculator object
+
+public class CalculatorSumTest
+{
+    private readonly Calculator calculator = new Calculator();
+
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(2, 2, 4)]
+    [InlineData(3, -2, 1)]
+    public void Add_TwoNumbers_ReturnsSum(int firstNumber, int secondNumber, int expected)
+    {
+        var result = calculator.Calculate(firstNumber, secondNumber, OperatorEnum.sum);
+
+        Assert.Equal(expected, result);
+    }
+}
+
+public class CalculatorSubtractTest
+{
+    private readonly Calculator calculator = new Calculator();
+
+    [Theory]
+    [InlineData(1, 2, -1)]
+    [InlineData(2, 2, 0)]
+    [InlineData(3, 2, 1)]
+    public void Subtract_TwoNumbers_ReturnsDifference(int firstNumber, int secondNumber, int expected)
+    {
+        var result = calculator.Calculate(firstNumber, secondNumber, OperatorEnum.sub);
+
+        Assert.Equal(expected, result);
+    }
+}
+
+public class CalculatorMultiplyTest
+{
+    private readonly Calculator calculator = new Calculator();
+    
+    [Theory]
+    [InlineData(1, 2, 2)]
+    [InlineData(2, 2, 4)]
+    [InlineData(-999, 0, 0)]
+    [InlineData(3, 2, 6)]
+    public void Multiply_TwoNumbers_ReturnsProduct(int firstNumber, int secondNumber, int expected)
+    {
+        var result = calculator.Calculate(firstNumber, secondNumber, OperatorEnum.multiply);
+
+        Assert.Equal(expected, result);
+    }
+}
+
+public class CalculatorDivisonTest
+{
+    private readonly Calculator calculator = new Calculator();
+    
+    [Theory]
+    [InlineData(1, 2, 0.5)]
+    [InlineData(2, 2, 1)]
+    [InlineData(3, 2, 1.5)]
+    public void Divide_TwoNumbers_ReturnsQuotient(int firstNumber, int secondNumber, double expected)
+    {
+        var result = calculator.Calculate(firstNumber, secondNumber, OperatorEnum.division);
+
+        Assert.Equal(expected, result);
+    }
+}
---
+++ C# Star Practices/SimpleCalculator/SimpleCalculator.Tests/Calculator.Operators.Test.cs
@@ -0,0 +1,68 @@
+namespace SimpleCalculator.Tests;
+
+using SimpleCalculator.Business;
+using SimpleCalculator.Business.OperatorBusiness;
+using SimpleCalculator.Business.OperatorBusiness.Operators;
+
+// test base operators
+
+public class OperatorSumTest
+{
+    private readonly SumOperator sumOperator = new SumOperator();
+
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(2, 2, 4)]
+    [InlineData(3, -2, 1)]
+    public void Add_TwoNumbers_ReturnsSum(int firstNumber, int secondNumber, int expected)
+    {
+        var result = this.sumOperator.Calculate(firstNumber, secondNumber);
+        Assert.Equal(expected, result);
+    }
+}
+
+public class OperatorSubTest
+{
+    private readonly SubOperator subOperator = new SubOperator();
+
+    [Theory]
+    [InlineData(1, 2, -1)]
+    [InlineData(2, 2, 0)]
+    [InlineData(3, 2, 1)]
+    public void Subtract_TwoNumbers_ReturnsDifference(int firstNumber, int secondNumber, int expected)
+    {
+        var result = this.subOperator.Calculate(firstNumber, secondNumber);
+        Assert.Equal(expected, result);
+    }
+}
+
+public class OperatorMultiplyTest
+{
+    private readonly MultiplyOperator multOperator = new MultiplyOperator();
+    
+    [Theory]
+    [InlineData(1, 2, 2)]
+    [InlineData(2, 2, 4)]
+    [InlineData(-999, 0, 0)]
+    [InlineData(3, 2, 6)]
+    public void Multiply_TwoNumbers_ReturnsProduct(int firstNumber, int secondNumber, int expected)
+    {
+        var result = this.multOperator.Calculate(firstNumber, secondNumber);
+        Assert.Equal(expected, result);
+    }
+}
+
+public class OperatorDivisonTest
+{
+    private readonly DivisionOperator divOperator = new DivisionOperator();
+    
+    [Theory]
+    [InlineData(1, 2, 0.5)]
+    [InlineData(2, 2, 1)]
+    [InlineData(3, 2, 1.5)]
+    public void Divide_TwoNumbers_ReturnsQuotient(int firstNumber, int secondNumber, double expected)
+    {
+        var result = this.divOperator.Calculate(firstNumber, secondNumber);
+        Assert.Equal(expected, result);
+    }
+}
