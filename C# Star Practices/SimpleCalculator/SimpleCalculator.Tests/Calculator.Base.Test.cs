72590b2be8b75b4a81eb4ee36b931ab1b87edb3e
Taha Alikhani <31014328+Taha-Alikhani@users.noreply.github.com>
2022-07-29 22:44:52 +0430

    Implemented mocking

--- C# Star Practices/SimpleCalculator/SimpleCalculator.Tests/Calculator.Base.Test.cs
+++ C# Star Practices/SimpleCalculator/SimpleCalculator.Tests/Calculator.Base.Test.cs
@@ -2,12 +2,22 @@ namespace SimpleCalculator.Tests;
 
 using SimpleCalculator.Business;
 using SimpleCalculator.Business.Enums;
+using SimpleCalculator.Business.Abstraction;
+using SimpleCalculator.Business.OperatorBusiness;
+using SimpleCalculator.Business.OperatorBusiness.Operators;
 
-// test Calculator object
+// test Calculator object, mocking the provider to make sure it's working as it should, and test it somewhere else
 
 public class CalculatorSumTest
 {
-    private readonly Calculator _calculator = new Calculator();
+    private readonly Calculator _calculator;
+
+    public CalculatorSumTest()
+    {
+        var mockedOperatorProvider = new Mock<IOperatorProvider>();
+        mockedOperatorProvider.Setup(x => x.GetOperator(OperatorEnum.sum)).Returns(new SumOperator());
+        this._calculator = new Calculator(mockedOperatorProvider.Object);
+    }
 
     [Theory]
     [InlineData(1, 2, 3)]
@@ -16,14 +26,20 @@ public class CalculatorSumTest
     public void Add_TwoNumbers_ReturnsSum(int firstNumber, int secondNumber, int expected)
     {
         var result = _calculator.Calculate(firstNumber, secondNumber, OperatorEnum.sum);
-
         Assert.Equal(expected, result);
     }
 }
 
 public class CalculatorSubtractTest
 {
-    private readonly Calculator _calculator = new Calculator();
+    private readonly Calculator _calculator;
+
+    public CalculatorSubtractTest()
+    {
+        var mockedOperatorProvider = new Mock<IOperatorProvider>();
+        mockedOperatorProvider.Setup(x => x.GetOperator(OperatorEnum.sub)).Returns(new SubOperator());
+        this._calculator = new Calculator(mockedOperatorProvider.Object);
+    }
 
     [Theory]
     [InlineData(1, 2, -1)]
@@ -39,7 +55,14 @@ public class CalculatorSubtractTest
 
 public class CalculatorMultiplyTest
 {
-    private readonly Calculator _calculator = new Calculator();
+    private readonly Calculator _calculator;
+
+    public CalculatorMultiplyTest()
+    {
+        var mockedOperatorProvider = new Mock<IOperatorProvider>();
+        mockedOperatorProvider.Setup(x => x.GetOperator(OperatorEnum.multiply)).Returns(new MultiplyOperator());
+        this._calculator = new Calculator(mockedOperatorProvider.Object);
+    }
     
     [Theory]
     [InlineData(1, 2, 2)]
@@ -56,7 +79,14 @@ public class CalculatorMultiplyTest
 
 public class CalculatorDivisonTest
 {
-    private readonly Calculator _calculator = new Calculator();
+    private readonly Calculator _calculator;
+
+    public CalculatorDivisonTest()
+    {
+        var mockedOperatorProvider = new Mock<IOperatorProvider>();
+        mockedOperatorProvider.Setup(x => x.GetOperator(OperatorEnum.division)).Returns(new DivisionOperator());
+        this._calculator = new Calculator(mockedOperatorProvider.Object);
+    }
     
     [Theory]
     [InlineData(1, 2, 0.5)]
